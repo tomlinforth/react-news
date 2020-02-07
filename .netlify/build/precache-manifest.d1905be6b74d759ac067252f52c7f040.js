73305c07d0bc28cdad15ca9f587e43737f33e42e
Tom Linforth <tom.linforth1@gmail.com>
2020-02-07 12:57:32 +0000

    hosted, changed readme7

---
+++ .netlify/build/precache-manifest.d1905be6b74d759ac067252f52c7f040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953ec80b92d6b72ef4d676a86860a2f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e48b5583614c25807f96",
+    "url": "/static/css/main.1b6abe70.chunk.css"
+  },
+  {
+    "revision": "844d6a8095e1d7cf5edd",
+    "url": "/static/js/2.393b7ab3.chunk.js"
+  },
+  {
+    "revision": "eee9ad91ef84ed45eebd56179d5a21db",
+    "url": "/static/js/2.393b7ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48b5583614c25807f96",
+    "url": "/static/js/main.64175a49.chunk.js"
+  },
+  {
+    "revision": "0b3194a6946273502a2f",
+    "url": "/static/js/runtime-main.65f9d357.js"
+  }
+]);
